6d2b5ab35de125efd530ca0ca19610b95dbea7d2
Sean Oughton <seanoughton@gmail.com>
2018-05-10 21:50:23 +0000

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -4,10 +4,19 @@ module ArtistsHelper
   end
 
   def artist_select(song,artist)
+    #if there is an artist already, then just show the artist, otherwise provide a list of ArtistsHelper
+
+    if artist
+      artist.name
+    else
+      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+    end
+=begin
     if song.artist.nil?
          select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
      else
        hidden_field_tag "song[artist_id]", song.artist_id
      end
+=end
   end
 end
